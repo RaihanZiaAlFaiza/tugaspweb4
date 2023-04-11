8da073d8dc3fe47ce5e6050001f2c0fa81a31555
RaihanZiaAlFaiza <129245944+RaihanZiaAlFaiza@users.noreply.github.com>
2023-04-11 23:21:39 +0700

    Update home.vue

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,17 +1,22 @@
 <template>
-template>
   <!--banner image-->
 
   <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
-</div>
-    <div class="content text-center">
-      <h1 class="text-white">Welcome To Nova Siska Nurfadilah</h1>
-    
+    <div  class="images">
+      <div class="profile">
+      <img src="../assets/profile2.png" class="rounded-circle"
+           height="200px" width="200px">
     </div>
-  
+    <div class="content text-center">
+      <h1 class="text-white">Raihan Zia Al-faiza</h1>
+      <h2>Aku lagi puasa ges</h2>
+    </div>
+    </div>
+  </div>
 
 
 </template>
+
 <script>
 export default {
   name: 'App',
@@ -22,8 +27,16 @@ export default {
 </script>
 
 <style>
-.banner-image{
-  background: dodgerblue;
+.text-center{
+  margin-top: 50px;
 }
+.images {
+  margin-top: -400px;
+}
+.banner-image{
+  background: #282828;
+  background-size: cover;
+}
+
 
 </style>
