81864b1562746dfb63e037fe78e738be73681b2c
Raihanzia <raihanziaalfaiza@gmail.com>
2023-04-11 17:12:26 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"><router-link to="/" style="color: black;">Home</router-link></a>
+          </li>
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#"><router-link to="/about" style="color: black;">About</router-link></a>
+          </li>
+      </ul>
+      
+    </div>
+  </div>
+</nav>
+<router-vue></router-vue>
+</template>
+
+<script>
+import home from "@/views/home.vue";
+export default{
+    name: 'App',
+    components:{
+      home
+    }
+}
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,29 @@
+<template>
+template>
+  <!--banner image-->
+
+  <div class="banner-image w-100 vh-100 d-flex justify-content-center align-items-center">
+</div>
+    <div class="content text-center">
+      <h1 class="text-white">Welcome To Nova Siska Nurfadilah</h1>
+    
+    </div>
+  
+
+
+</template>
+<script>
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+.banner-image{
+  background: dodgerblue;
+}
+
+</style>
